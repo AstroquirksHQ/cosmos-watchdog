70a5d5926a3a2df15a88cec181aca7c2a714e85a
Malik FASSI <malikfassifihri@gmail.com>
2023-03-09 00:22:01 +0100

    flask host & port fix

--- tests/integration/test_health.py
+++ tests/integration/test_health.py
@@ -4,6 +4,6 @@ import json
 
 def test_get_health():
     response = requests.get("http://localhost:5000/_health")
-    data = json.loads(response.text)
     assert response.status_code == 200
+    data = json.loads(response.text)
     assert data["success"] == "True"
